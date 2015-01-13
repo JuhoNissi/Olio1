fc7c61d4315b2e5c247ebdf3c8cc7d54b1427183
Juho Nissi <juho_356@hotmail.com>
2015-01-13 11:08:09 +0200

    Demo01

---
+++ Demo01/src/fi/jamk/Tehtava1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fi.jamk;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author h9321
+ */
+public class Tehtava1 {
+    public static void main(String args[]){
+        //kysy pistemäärä
+        int pisteet;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Anna pisteet > ");
+        pisteet = scanner.nextInt();
+        //tulosta arvosana
+        switch(pisteet){
+            case 1: 
+        }
+    }
+}
